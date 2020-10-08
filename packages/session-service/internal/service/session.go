7968a974e41ee58e5229d80ebf8699a3dd6b3721
al-gee <accs.x@pm.me>
2020-10-08 19:57:55 +0200

    Remove redundant log.Printf

--- packages/session-service/internal/service/session.go
+++ packages/session-service/internal/service/session.go
@@ -2,17 +2,13 @@ package service
 
 import (
 	"github.com/google/uuid"
-	"log"
 )
-
 
 func (api *API) GetSessionById(id string) (*Session, error) {
 	s := Session{ID: id}
 	if err := api.db.Preload("Speakers").Preload("Event.Sessions").Preload("Comments").First(&s).Error; err != nil {
 		return nil, err
 	}
-
-	log.Printf("session: %+v", s)
 	return &s, nil
 }
 
