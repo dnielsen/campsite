90a360e87a0dc18d16245bfb6900cb6a1e661a5f
Al Gee <al.gee.engineer@icloud.com>
2020-10-26 00:38:37 +0100

    Update go modules

--- services/api/main.go
+++ services/api/main.go
@@ -87,10 +87,12 @@ func main() {
 
 	// Set up the server.
 	corsWrapper := cors.New(cors.Options{
-		AllowCredentials: true,
-		AllowedOrigins: []string{"http://localhost:3000", "http://localhost:1111"},
-		AllowedMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete},
-		AllowedHeaders: []string{"Content-Type", "Origin", "Accept", "*", "Authorization", "Cookie"},
+		AllowedOrigins:         []string{"http://localhost:3000", "http://localhost:1111"},
+		AllowedMethods:         []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete},
+		AllowedHeaders:         []string{"Content-Type", "Origin", "Accept", "*", "Authorization", "Cookie", "Set-Cookie"},
+		ExposedHeaders:         []string{"Set-Cookie"},
+		MaxAge:                 999999,
+		AllowCredentials:       true,
 	})
 
 	// Set up the server.
