9e35b1cb244a64e380dc11627fe7057ffe5ff80b
Al Gee <al.gee.engineer@icloud.com>
2020-10-26 23:36:11 +0100

    Update pkg

--- pkg/tracing/tracing.go
+++ pkg/tracing/tracing.go
@@ -20,7 +20,10 @@ func NewTracer(serviceName string, servicePort int, c *config.Config) *zipkin.Tr
 
 	// Local endpoint represents the local service information.
 	// The port needs to be of type `uint16` so we're converting it.
-	localEndpoint := &model.Endpoint{ServiceName: serviceName, Port: uint16(servicePort)}
+	localEndpoint := &model.Endpoint{
+		ServiceName: serviceName,
+		Port:        uint16(servicePort),
+	}
 
 	// TRACE_RECORD_RATE is a float from 0 to 1.
 	// 1 means 100% of traces will be recorded.
@@ -30,7 +33,9 @@ func NewTracer(serviceName string, servicePort int, c *config.Config) *zipkin.Tr
 	}
 
 	// Create a new tracer.
-	t, err := zipkin.NewTracer(reporter, zipkin.WithSampler(sampler), zipkin.WithLocalEndpoint(localEndpoint))
+	t, err := zipkin.NewTracer(reporter, zipkin.WithSampler(sampler), zipkin.WithLocalEndpoint(localEndpoint), zipkin.WithTags(map[string]string{
+		"hi": "hello",
+	}))
 	if err != nil {
 		log.Fatalf("Failed to create tracer: %v", err)
 	}
