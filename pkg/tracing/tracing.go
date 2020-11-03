88c0951a1318d1b8584307a853778a6e92006355
Al Gee <al.gee.engineer@icloud.com>
2020-11-03 22:34:01 +0100

    Update tracing

--- pkg/tracing/tracing.go
+++ pkg/tracing/tracing.go
@@ -18,7 +18,7 @@ func NewTracer(serviceName string, servicePort string, c *config.Config) *zipkin
 	reporter := reporterHttp.NewReporter(endpointUrl)
 
 	// Local endpoint represents the local service information.
-	localEndpoint, err := zipkin.NewEndpoint(serviceName, fmt.Sprintf("localhost:%s", servicePort))
+	localEndpoint, err := zipkin.NewEndpoint(serviceName, fmt.Sprintf("%s:%s", c.Tracing.Host, servicePort))
 	if err != nil {
 		log.Printf("Failed to create endpoint: %v", err)
 	}
